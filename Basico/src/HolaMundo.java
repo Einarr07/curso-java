71c705249d02f6534175a8e0c341040d2bd3bccb
Einarr07 <aidenperse@gmail.com>
2024-07-12 12:04:18 -0500

    Mi primer Hola Mundo en Java

---
+++ Basico/src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Comentario de una sola linea
+        System.out.println("Hola mundo");
+        /*Este es un comentario de multiples lineas
+        Para agg una explicación más extensa
+        */
+    }
+}
