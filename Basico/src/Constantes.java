b5a239c7e8d0541cb471efca854d5e9e6fb89064
Einarr07 <aidenperse@gmail.com>
2024-07-15 12:41:30 -0500

    Como declarar constantes en java

---
+++ Basico/src/Constantes.java
@@ -0,0 +1,11 @@
+public class Constantes {
+    public static void main(String[] args) {
+
+        // Declaración de una constante de tipo float con el valor de pi
+        // La palabra clave 'final' indica que el valor de 'pi' no puede cambiar después de ser inicializado
+        final float pi = 3.14159265359f;
+
+        // Imprimir el valor de la constante 'pi' en la consola
+        System.out.println("El número de pi es: " + pi);
+    }
+}
