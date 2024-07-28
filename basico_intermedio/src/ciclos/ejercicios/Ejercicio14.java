65de9d6a3f501d1a2b595388c6715950e95b55e6
Einarr07 <aidenperse@gmail.com>
2024-07-27 20:00:43 -0500

    Ejercicio 14 completado

---
+++ basico_intermedio/src/ciclos/ejercicios/Ejercicio14.java
@@ -0,0 +1,21 @@
+package ciclos.ejercicios;
+
+import javax.swing.*;
+
+public class Ejercicio14 {
+    public static void main(String[] args) {
+        float total = 0;
+        int contadorMayorMil = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            float sueldo = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el sueldo Nº " + i));
+            total += sueldo;
+            if (sueldo > 1000) {
+                contadorMayorMil++;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "La suma de los 10 sueldos es: $" + total + " dólares\n" +
+                "Y " + contadorMayorMil + " son mayores a $1000 (mil dólares)");
+    }
+}
